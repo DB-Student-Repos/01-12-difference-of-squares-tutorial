12e3f9bf1a2d44d675464475ad539ca500e5a903
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-03 19:33:50 +0100

    Update difference_of_squares.py

--- difference_of_squares.py
+++ difference_of_squares.py
@@ -1,10 +1,16 @@
 def square_of_sum(number):
-    pass
+    x= (number*(number+1))/2
+    return x*x
+    
 
 
 def sum_of_squares(number):
-    pass
+    x= (number*(number+1)*(2*number+1))/6
+    return x
+    
 
 
 def difference_of_squares(number):
-    pass
+    x= (square_of_sum(number)-sum_of_squares(number))
+    return x
+    
